3a2b4950455547a74d511834d6fa5edf464ec9f5
forcode07 <159250770+forcode07@users.noreply.github.com>
2024-02-07 17:56:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you 9th!";
+  question.innerHTML = "Yayyy!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
